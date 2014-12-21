c23c996a2567cdd66ba08cceecbb70416107d586
Dave Simel <davesimel@gmail.com>
2014-12-21 19:42:26 +0000

    added new scaffold db for purchases

---
+++ db/migrate/20141221193820_create_purchases.rb
@@ -0,0 +1,13 @@
+class CreatePurchases < ActiveRecord::Migration
+  def change
+    create_table :purchases do |t|
+      t.string :name
+      t.string :category
+      t.string :string
+      t.integer :quantity
+      t.references :invoice, index: true
+
+      t.timestamps
+    end
+  end
+end
